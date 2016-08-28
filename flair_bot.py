5830debce41d155b4e28556799e09b5556cbc688
nosas <sasonn@live.com>
2016-08-27 22:02:50 -0700

    v1.0 of the_flair_bot is ready!
    Added:
    - Reading/writing the removed and processed submissions to files
    - Comprehensive logging
    - More code cleanup
    
    TODO:
    - Add signature to the bot's message to notify user that it's a bot created by /u/nosas
    - More documentation, block comment at top describing program and methods
    - Make it more customizable for future users/developers
    - Allow users to reply to bot and have the bot set specific flair
    - Functionality to make bot read mod mail and approve posts if they've enabled flair

--- flair_bot.py
+++ flair_bot.py
@@ -5,50 +5,43 @@ from login import reddit_login
 
 """
 TODO:
-    # - Figure out how to approve posts
-    # - Create message outline to send to user (probably copy from old flair bot)
-    # - Add some proper documentation
-    # - Push to GitHub/fix Repo
-    - Need cleaner, more informative documentation
-    - Fix the removal of post IDs from removed_submissions list
-    - Automate the process
     - Allow bot to run for certain # of cycles, then clean memory after # of cycles to prevent eating of memory
-    - Add customizable bot options for potential future users
+    - Add customizable bot options for potential future users/developers?
     - Create functionality for users to reply to bot and ask for a specific flair
 """
 
 subreddit_name = "NosasFlairTest"
-
-add_flair_subject = ""
-add_flair_body = ""
+removed_submissions_filename = "removed_submissions.txt"
+processed_submissions_filename = "processed_submissions.txt"
 
 remove_post_subject = "Your post requires a flair."
 remove_post_body = "[Your recent post]({0}) in /r/{1} does not have any flair and has been removed. " \
                     "Please add flair to the post and [message the moderators with this message.]({2})"
 
 mod_mail_subject = "Flair Added"
-# mod_mail_body = "Replace this text with your post's url."
 
 
-class Bot():
+class Bot:
     # Global variables to keep track of submissions that are removed/resolved
     removed_submissions = []
     processed_submissions = []
 
     # Initialize Reddit
     r = praw.Reddit(user_agent="A flair mod by /u/nosas for /r/NosasFlairTest")
-    # reddit_login(r)
     print "Logging in ...\n"
+    reddit_login(r)
     subreddit = r.get_subreddit(subreddit_name)
+
     # Grab the 10 most recent posts on the subreddit and check the submissions for flairs
     def check_new_submissions(self):
-        # Grab the 10 most recents submissions
-        for submission in self.subreddit.get_new(limit=1):
-            print "Getting new submissions ..."
+        # Grab the 10 most recent submissions
+        print "    Getting new submissions ..."
+        for submission in self.subreddit.get_new(limit=10):
             # If the current post hasn't been processed yet, then see if the OP has a flair
             if submission.id not in self.processed_submissions:
                 # If OP has no flair, remove submission, notify user of removal, and add ID to removed_submission list
                 if submission.author_flair_text is None:
+                    # Make sure the author of the post isn't [deleted]
                     try:
                         author = submission.author
                         if author is None:
@@ -57,18 +50,18 @@ class Bot():
                         # Author is deleted. Disregard post.
                         continue
 
-                    # Notify user why their post was deleted
-                    # self.r.send_message(author, remove_post_subject, remove_post_body.format(
-                    #     submission.url, subreddit_name, self.create_mod_mail_url(submission.url)))
-                    print "Sending message to author ..."
                     # Delete the post and add the ID to the removed_submissions list
-                    # submission.remove()
-                    print "Removed submission: {0}".format(submission)
+                    submission.remove()
+                    print "    Removed submission: {0}".format(submission)
                     self.removed_submissions.append(submission.id)
-        print "Finished new submissions"
+                    # Notify user why their post was deleted
+                    self.r.send_message(author, remove_post_subject, remove_post_body.format(
+                        submission.url, subreddit_name, self.create_mod_mail_url(submission.url)))
+                    print "    Sending message to author ..."
 
-    # Check the removed submission to see if they've added flairs yet
-    # If they have, then approve the submission.
+        print "    Finished new submissions"
+
+    # Check the removed submissions to see if they've added flairs yet. If they have, then approve the submission.
     def check_removed_submissions(self):
         # Check each post from the removed_submissions list for flairs
         for submission_id in self.removed_submissions:
@@ -78,25 +71,68 @@ class Bot():
 
             # If OP has a flair now, approve their post, add the ID to processed_submissions list
             # This will prevent it from being processed/removed again.
-            # TODO: Change to None instead of not None
             if submission.author_flair_text is not None:
-                # submission.approve()
-                print "Approved submission: {0}".format(submission)
+                submission.approve()
+                print "    Approved submission: {0}".format(submission)
                 self.processed_submissions.append(submission_id)
-        print "Finished removed submissions"
 
+        print "    Finished removed submissions"
 
-    def create_mod_mail_url(self, post_url):
+    # Method to create a URL that the user can easily send to mods' mailboxes to access their removed submission
+    def create_mod_mail_url(self, submission_url):
         mod_mail_url = "http://www.reddit.com/message/compose/?to=/r/{0}&subject={1}&message={2}"\
-                        .format(subreddit_name, mod_mail_subject, post_url)
+                        .format(subreddit_name, mod_mail_subject, submission_url)
         return mod_mail_url
 
+    # Will remove IDs from removed_submissions list if they've been processed/approved
+    def update_removed_submissions(self):
+        new_removed_submissions = [item for item in self.removed_submissions if item not in self.processed_submissions]
+        self.removed_submissions = new_removed_submissions
+
+    # Opens a file and reads the list from it in order to retrieve the removed/processed_submissions list
+    def open_submissions_list(self, submissions_file):
+        try:
+            with open(submissions_file, 'r') as f:
+                try:
+                    # Read file. If text inside is a list, proceed. Else, raise error and default the variable to list.
+                    submissions_list = eval(f.read())
+                    if not isinstance(submissions_list, list):
+                        raise SyntaxError
+                except SyntaxError:
+                    submissions_list = []
+        # If it doesn't exist, then create it
+        except IOError:
+            open(submissions_file, 'w').close()
+            submissions_list = []
+
+        return submissions_list
+
+    # Write the removed/processed_submissions to their
+    def write_submissions_list_to_file(self, submissions_list, submissions_file):
+        with open(submissions_file, 'w') as f:
+            f.write(str(submissions_list))
+
     def run(self):
+        print "Retrieving removed submissions from file ..."
+        self.removed_submissions = self.open_submissions_list(removed_submissions_filename)
+
+        print "Retrieving processed submissions from file ..."
+        self.processed_submissions = self.open_submissions_list(processed_submissions_filename)
+
         print "Checking new submissions ..."
         self.check_new_submissions()
 
         print "Checking removed submissions ..."
         self.check_removed_submissions()
+
+        print "Updating removed submissions list ..."
+        self.update_removed_submissions()
+
+        print "Writing processed submissions to file ..."
+        self.write_submissions_list_to_file(self.processed_submissions, processed_submissions_filename)
+
+        print "Writing removed submissions to file ..."
+        self.write_submissions_list_to_file(self.removed_submissions, removed_submissions_filename)
 
 # Master bot process
 if __name__ == '__main__':
@@ -104,12 +140,6 @@ if __name__ == '__main__':
     while True:
         print('{:%H:%M:%S %b %d %Y}'.format(datetime.datetime.now()))
         reddit_bot.run()
-        print("Sleeping for 60 seconds\n")
+        print("Sleeping for 10 minutes\n")
         print "= = " * 10
-        time.sleep(60)
-
-# new_removed_submissions = [item for item in removed_submissions if item not in processed_submissions]
-# removed_submissions = new_removed_submissions
-# print "Finished : " + str(processed_submissions)
-# print "New List : " + str(new_removed_submissions)
-# print "Removed  : " + str(removed_submissions)
+        time.sleep(600)
