ab6d55e920b2f213ba697a0f21330bfe9485c72c
nosas <sasonn@live.com>
2016-08-27 17:38:37 -0700

    - Removed some unnecessary/old code - Added some error exceptions - Added some automation     + More automation is definitely needed - Updated logging     + More is definitely needed

--- flair_bot.py
+++ flair_bot.py
@@ -1,14 +1,19 @@
 import time
 import praw
-from Projects.RedditBot.login import reddit_login
+from login import reddit_login
 
 """
 TODO:
     # - Figure out how to approve posts
     # - Create message outline to send to user (probably copy from old flair bot)
     # - Add some proper documentation
-        + Need cleaner documentation. Will add tomorrow.
-    - Push to GitHub/fix Repo
+    # - Push to GitHub/fix Repo
+    - Need cleaner, more informative documentation
+    - Fix the removal of post IDs from removed_submissions list
+    - Automate the process
+    - Allow bot to run for certain # of cycles, then clean memory after # of cycles to prevent eating of memory
+    - Add customizable bot options for potential future users
+    - Create functionality for users to reply to bot and ask for a specific flair
 """
 
 subreddit_name = "NosasFlairTest"
@@ -18,16 +23,16 @@ add_flair_body = ""
 
 remove_post_subject = "Your post requires a flair."
 remove_post_body = "[Your recent post]({0}) in /r/{1} does not have any flair and has been removed. " \
-                    "Please add flair to the post and [message the moderators]({2}) with a link to your post."
+                    "Please add flair to the post and [message the moderators with this message.]({2})"
 
 mod_mail_subject = "Flair Added"
-mod_mail_body = "Replace this text with your post's url."
+# mod_mail_body = "Replace this text with your post's url."
 
 
 class Bot():
     # Global variables to keep track of submissions that are removed/resolved
     removed_submissions = []
-    finished_submissions = []
+    processed_submissions = []
 
     # Initialize Reddit
     r = praw.Reddit(user_agent="A flair mod by /u/nosas for /r/NosasFlairTest")
@@ -42,19 +47,27 @@ class Bot():
             print submission
 
             # If the current post hasn't been processed yet, then see if the OP has a flair
-            if submission.id not in self.finished_submissions:
+            if submission.id not in self.processed_submissions:
                 # If OP has no flair, remove submission, notify user of removal, and add ID to removed_submission list
-                #TODO: Change to None instead of not None
+                # TODO: Change to None instead of not None
                 if submission.author_flair_text is not None:
-                    author = submission.author
-                    #TODO: Send a more elegant message
+                    try:
+                        author = submission.author
+                        if author is None:
+                            raise AttributeError
+                    except AttributeError:
+                        # Author is deleted. Disregard post.
+                        continue
+
+                    # Notify user why their post was deleted
                     self.r.send_message(author, remove_post_subject, remove_post_body.format(
-                        submission.url, subreddit_name, self.create_mod_mail_url(submission.url)
-                    ))
-                    # self.r.send_message(author, "Post removed.", "Your post has been removed until you've put on a flair")
+                        submission.url, subreddit_name, self.create_mod_mail_url(submission.url)))
+
+                    # Delete the post and add the ID to the removed_submissions list
                     submission.remove()
                     self.removed_submissions.append(submission.id)
-                    print "Removed {0} \n ID: {1}".format(submission.title, submission.id)
+                    print "REMOVED"
+                    print "ID: {0}".format(submission.id)
 
     # Check the removed submission to see if they've added flairs yet
     # If they have, then approve the submission.
@@ -68,39 +81,43 @@ class Bot():
             print "---" * 6
             print submission
 
-            # If OP has a flair now, approve their post, add the ID to finished_submissions list
+            # If OP has a flair now, approve their post, add the ID to processed_submissions list
             # This will prevent it from being processed/removed again.
-            #TODO: Change to None instead of not None
+            # TODO: Change to None instead of not None
             if submission.author_flair_text is not None:
                 submission.approve()
-                self.finished_submissions.append(submission_id)
-                print "Approved " + str(submission)
+                self.processed_submissions.append(submission_id)
+                print "APPROVED"
+                print "ID: {0}".format(submission.id)
+
 
     def create_mod_mail_url(self, post_url):
         mod_mail_url = "http://www.reddit.com/message/compose/?to=/r/{0}&subject={1}&message={2}"\
-                        .format(subreddit_name, mod_mail_subject, mod_mail_body)
-        remove_post_message = "[Your recent post]({0}) in /r/{1} still does not have any flair and has been removed. " \
-                              "Please add flair to the post and [message the moderators]({2}) with a link to your post."\
-                              .format(post_url, subreddit_name, mod_mail_url)
+                        .format(subreddit_name, mod_mail_subject, post_url)
         return mod_mail_url
 
     def run(self):
+        print
+        print "===" * 8
+        print "CHECKING NEW SUBMISSIONS"
         self.check_new_submissions()
+
+        print
+        print "===" * 8
+        print "\nCHECKING REMOVED SUBMISSIONS"
         self.check_removed_submissions()
 
 
 # Master bot process
 if __name__ == '__main__':
     reddit_bot = Bot()
+    while True:
+        reddit_bot.run()
+        print("Sleeping for 10 seconds")
+        time.sleep(10)
 
-    reddit_bot.run()
-
-
-# main()
-#
-#
-# new_removed_submissions = [item for item in removed_submissions if item not in finished_submissions]
+# new_removed_submissions = [item for item in removed_submissions if item not in processed_submissions]
 # removed_submissions = new_removed_submissions
-# print "Finished : " + str(finished_submissions)
+# print "Finished : " + str(processed_submissions)
 # print "New List : " + str(new_removed_submissions)
 # print "Removed  : " + str(removed_submissions)
