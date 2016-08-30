325003874d0345500a68c1c4bf712be1a6d19dbe
nosas <sasonn@live.com>
2016-08-29 23:44:25 -0700

    Added more documentation.
    TODO:
    - Create a README
    - Maybe clean up methods so not every method is running in bot.run()

--- flair_bot.py
+++ flair_bot.py
@@ -1,22 +1,35 @@
+"""
+Author: Sason R Baghdadi
+Purpose: Check new submissions on Reddit.com/TeenWolf for users without flairs
+
+Actions required to implement:
+    1. Python 2.7 (Haven't tested it on Python 3.x yet)
+    2. A Reddit account with moderator permission on your subreddit.
+        2a. The account will need at least 2 link karma in order to bypass CAPTCHA requirements.
+    3. The PRAW module will need to be installed on the machine running this script.
+        3a. PRAW can be found here: https://praw.readthedocs.org/en/latest/
+            Don't know how to install Python modules on Windows? Watch this: http://www.youtube.com/watch?v=ddpYVA-7wq4
+"""
 import time
-import datetime
+from datetime import datetime
 import praw
 from login import reddit_login
 
 """
 TODO:
-    - Allow bot to run for certain # of cycles, then clean memory after # of cycles to prevent eating of memory
-    - Add customizable bot options for potential future users/developers?
     - Create functionality for users to reply to bot and ask for a specific flair
+    - Add customizable bot options for potential future users/developers?
+    - Allow bot to run for certain # of cycles, then clean memory after # of cycles to prevent eating of memory??
 """
 
-subreddit_name = "NosasFlairTest"
+# Global variable, designed to be customizable for user
+SUBREDDIT_NAME = "NosasFlairTest"
 removed_submissions_filename = "removed_submissions.txt"
 processed_submissions_filename = "processed_submissions.txt"
 
 remove_post_subject = "Your post requires a flair."
-remove_post_body = "[Your recent post]({0}) in /r/{1} does not have any flair and has been removed. " \
-                    "Please add flair to the post and [message the moderators with this message.]({2})"
+remove_post_body = "[Your recent post]({url}) in /r/{subreddit} does not have any flair and has been removed. " \
+                    "Please add flair to the post and [message the moderators with this message.]({msg})"
 
 mod_mail_subject = "Flair Added"
 
@@ -32,7 +45,7 @@ class Bot:
         self.r = praw.Reddit(user_agent="A flair mod by /u/nosas for /r/NosasFlairTest")
         print "Logging in ...\n"
         reddit_login(self.r)
-        self.subreddit = self.r.get_subreddit(subreddit_name)
+        self.subreddit = self.r.get_subreddit(SUBREDDIT_NAME)
 
     # Grab the 10 most recent posts on the subreddit and check the submissions for flairs
     def check_new_submissions(self):
@@ -58,7 +71,7 @@ class Bot:
                     self.removed_submissions.append(submission.id)
                     # Notify user why their post was deleted
                     self.r.send_message(author, remove_post_subject, remove_post_body.format(
-                        submission.url, subreddit_name, self.create_mod_mail_url(submission.url)))
+                        url=submission.url, subreddit=SUBREDDIT_NAME, msg=self.create_mod_mail_url(submission.url)))
                     print "    Sending message to author ..."
 
         print "    Finished new submissions"
@@ -68,7 +81,7 @@ class Bot:
         # Check each post from the removed_submissions list for flairs
         for submission_id in self.removed_submissions:
             # Create a url with the subreddit and post ID in order to obtain PRAW's submission object
-            url = "https://www.reddit.com/r/{0}/comments/{1}".format(subreddit_name, submission_id)
+            url = "https://www.reddit.com/r/{0}/comments/{1}".format(SUBREDDIT_NAME, submission_id)
             submission = self.r.get_submission(url=url)
 
             # If OP has a flair now, approve their post, add the ID to processed_submissions list
@@ -84,7 +97,7 @@ class Bot:
     # Method to create a URL that the user can easily send to mods' mailboxes to access their removed submission
     def create_mod_mail_url(submission_url):
         mod_mail_url = "http://www.reddit.com/message/compose/?to=/r/{0}&subject={1}&message={2}"\
-                        .format(subreddit_name, mod_mail_subject, submission_url)
+                        .format(SUBREDDIT_NAME, mod_mail_subject, submission_url)
         return mod_mail_url
 
     # Will remove IDs from removed_submissions list if they've been processed/approved
@@ -104,7 +117,7 @@ class Bot:
                         raise SyntaxError
                 except SyntaxError:
                     submissions_list = []
-        # If it doesn't exist, then create it
+        # If the file doesn't exist, then create it
         except IOError:
             open(submissions_file, 'w').close()
             submissions_list = []
@@ -143,8 +156,8 @@ class Bot:
 if __name__ == '__main__':
     reddit_bot = Bot()
     while True:
-        print('{:%H:%M:%S %b %d %Y}'.format(datetime.datetime.now()))
+        print('{:%H:%M:%S %b %d %Y}'.format(datetime.now()))
         reddit_bot.run()
-        print("Sleeping for 10 minutes\n")
+        print("Sleeping for 5 minutes\n")
         print "= = " * 10
-        time.sleep(600)
+        time.sleep(60*5)
